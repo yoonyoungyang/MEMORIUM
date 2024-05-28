4149ac61838b8587d6e44db69cd5981530a64058
Yu Taek Lee <lcpnine@gmail.com>
2024-05-28 23:58:12 +0900

    :bug: Add background classnames on safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  safelist: [
+    'bg-red-500',
+    'bg-yellow-500',
+    'bg-blue-500',
+    'bg-purple-500',
+    'bg-green-500',
+  ],
   theme: {
     extend: {},
   },
