eddfcc06c176d6a208de18135f17b9772b6f12bc
yoonyoungyang <youngonly0118@gmail.com>
2024-05-29 21:59:13 +0900

    결과창 흩뿌리기 성공

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -26,10 +26,36 @@ ChartJS.register(
 const Result: React.FC = () => {
   const navigate = useNavigate()
   const { perfume } = usePerfume()
-  console.log('perfume: ', perfume)
-
   const handleNextClick = () => {
     navigate('/customizing')
+  }
+
+  const changeTopImgSource = () => {
+    if (perfume?.recipe.top.name === 'Figue')
+      return '/assets/img/recipe_img/Figue.png'
+    if (perfume?.recipe.top.name === 'Green Tea')
+      return '/assets/img/recipe_img/Greentea.png'
+    if (perfume?.recipe.top.name === 'Bitter Lemon')
+      return '/assets/img/recipe_img/Bitterlemon.png'
+    return ''
+  }
+  const changeMiddleImgSource = () => {
+    if (perfume?.recipe.middle.name === 'Ilang Ilang')
+      return '/assets/img/recipe_img/Ylangylang.png'
+    if (perfume?.recipe.middle.name === 'Sea Scent')
+      return '/assets/img/recipe_img/Seascent.png'
+    if (perfume?.recipe.middle.name === 'Freesia')
+      return '/assets/img/recipe_img/Freesia.png'
+    return ''
+  }
+  const changeBaseImgSource = () => {
+    if (perfume?.recipe.base.name === 'Musk')
+      return '/assets/img/recipe_img/Musk.png'
+    if (perfume?.recipe.base.name === 'Sandal Wood')
+      return '/assets/img/recipe_img/Sandalwood.png'
+    if (perfume?.recipe.base.name === 'Moss')
+      return '/assets/img/recipe_img/Moss.png'
+    return ''
   }
 
   const data = {
@@ -37,7 +63,13 @@ const Result: React.FC = () => {
     datasets: [
       {
         label: 'Scent Profile',
-        data: [8, 6, 5, 4, 7],
+        data: [
+          perfume?.visualizeData.Woody,
+          perfume?.visualizeData.Oriental,
+          perfume?.visualizeData.Fresh,
+          perfume?.visualizeData.Floral,
+          perfume?.visualizeData.Citrus,
+        ],
         backgroundColor: 'rgba(255, 255, 255, 0.1)',
         borderColor: 'rgba(255, 255, 255, 1)',
         borderWidth: 2,
@@ -76,104 +108,41 @@ const Result: React.FC = () => {
       },
     },
   }
-  const [changeTopImg, setChangeTopImg] = useState('Figue')
-  const topIngredient = ['Figue', 'Greentea', 'Bitterlemon']
-  const changeTopIngImg = (chooseTop: string) => {
-    setChangeTopImg(chooseTop)
-  }
-  const [changeMiddleImg, setChangeMiddleImg] = useState('Seascent')
-  const middleIngredient = ['Seascent', 'YlangYlang', 'Freesia']
-  const changeMiddleIngImg = (chooseMiddle: string) => {
-    setChangeMiddleImg(chooseMiddle)
-  }
-  const [changeBottomImg, setChangeBottomImg] = useState('Figue')
-  const bottomIngredient = ['Figue', 'Greentea', 'Bitterlemon']
-  const changeBottomIngImg = (chooseBottom: string) => {
-    setChangeBottomImg(chooseBottom)
-  }
   return (
     <div className="bg-gray-800 text-white min-h-screen p-8 font-serif">
       <div className="container mx-auto">
         <div className="flex flex-col md:flex-row items-center justify-between">
           <div className="md:w-1/2">
-            <h1 className="text-6xl font-bold">A Cool Dawn</h1>
-            <p className="text-2xl mt-2">서늘한 여명</p>
+            <h1 className="text-6xl font-bold">{perfume?.name.eng}</h1>
+            <p className="text-2xl mt-2">{perfume?.name.kor}</p>
             <p className="text-lg mt-8 leading-relaxed">
-              아침 이슬을 닮은 서늘함과 신선함을 표현한 향수입니다. Top 노트의
-              그린티가 청량한 아침을 깨우고, Middle 노트의 블랙베리가 달콤하고
-              풍부한 향을 더합니다. Base 노트로는 모스가 자연의 진한 향을
-              선사합니다.
+              {perfume?.description}
             </p>
             <h2 className="text-4xl font-bold mt-16">RECIPE</h2>
             <div className="flex justify-between mt-8">
               <div className="text-center">
                 <h3 className="text-2xl mb-4">TOP</h3>
                 <img
-                  src={`/assets/img/recipe_img/${changeTopImg}.png`}
+                  src={changeTopImgSource()}
                   alt="Top Note"
                   className="h-32 mx-auto"
                 />
-                <div className="flex space-x-4 pt-3">
-                  {topIngredient.map(function (IngEl) {
-                    return (
-                      <button
-                        title={`${IngEl}버튼`}
-                        type="button"
-                        id={`btn_${IngEl}`}
-                        onClick={() => changeTopIngImg(IngEl)}
-                        className={`h-12 w-full rounded-full bg-red-500`}
-                      >
-                        {IngEl}
-                      </button>
-                    )
-                  })}
-                </div>
               </div>
               <div className="text-center">
                 <h3 className="text-2xl mb-4">MIDDLE</h3>
                 <img
-                  src={`/assets/img/recipe_img/${changeMiddleImg}.png`}
+                  src={changeMiddleImgSource()}
                   alt="Middle Note"
                   className="h-32 mx-auto"
                 />
-                <div className="flex space-x-4 pt-3">
-                  {middleIngredient.map(function (IngEl) {
-                    return (
-                      <button
-                        title={`${IngEl}버튼`}
-                        type="button"
-                        id={`btn_${IngEl}`}
-                        onClick={() => changeMiddleIngImg(IngEl)}
-                        className={`h-12 w-full rounded-full bg-blue-500`}
-                      >
-                        {IngEl}
-                      </button>
-                    )
-                  })}
-                </div>
               </div>
               <div className="text-center">
                 <h3 className="text-2xl mb-4">BASE</h3>
                 <img
-                  src={`/assets/img/recipe_img/${changeBottomImg}.png`}
+                  src={changeBaseImgSource()}
                   alt="Base Note"
                   className="h-32 mx-auto"
                 />
-                <div className="flex space-x-4 pt-3">
-                  {bottomIngredient.map(function (IngEl) {
-                    return (
-                      <button
-                        title={`${IngEl}버튼`}
-                        type="button"
-                        id={`btn_${IngEl}`}
-                        onClick={() => changeBottomIngImg(IngEl)}
-                        className={`h-12 w-full rounded-full bg-purple-500`}
-                      >
-                        {IngEl}
-                      </button>
-                    )
-                  })}
-                </div>
               </div>
             </div>
           </div>
